c691827f680e4f2a265eca03fc9bb8be47c175cd
mubarismdeen <mubarisv@gmail.com>
2023-11-14 21:49:25 +0530

    corrections for changelog

--- src/main/resources/db/changesets/01_initialize_superman.sql
+++ src/main/resources/db/changesets/01_initialize_superman.sql
@@ -1,8 +1,12 @@
-insert into employees(create_by, create_date, edit_by, edit_date, id, emp_code, first_name) values(1, now(), 1, now(), 1, 1, 'superman');
-insert into users(create_by, create_date, edit_by, edit_date, id, emp_id, user_code, user_name, password) values(1, now(), 1, now(), 1, 1, 1, 'superman', '123');
-insert into user_screens(create_by, create_date, edit_by, edit_date, user_id, dashboard, stock, sales, customers, employees, attendance, salary_master, salary_payout, leave_salary, gratuity)
-            values(1, now(), 1, now(), 1, true, true, true, true, true, true, true, true, true, true);
-
-
-
-
+INSERT INTO departments(create_by, create_date, edit_by, edit_date,dept_name)
+	VALUES (1, now(), 1, now(),'admin');
+INSERT INTO countries(create_by, create_date, edit_by, edit_date,name)
+	VALUES (1, now(), 1, now(),'india');
+INSERT INTO statuses(create_by, create_date, edit_by, edit_date,status)
+	VALUES (1, now(), 1, now(),'Active');
+INSERT INTO employees(create_by, create_date, edit_by, edit_date, id, emp_code, first_name, department_id, nationality, status)
+    VALUES(1, now(), 1, now(), 1, '1', 'superman', 1, 1, 1);
+INSERT INTO users(create_by, create_date, edit_by, edit_date, id, emp_id, user_code, user_name, password)
+    VALUES(1, now(), 1, now(), 1, 1, 1, 'superman', '123');
+INSERT INTO user_screens(create_by, create_date, edit_by, edit_date, user_id, dashboard, stock, sales, customers, employees, attendance, salary_master, salary_payout, leave_salary, gratuity)
+            VALUES(1, now(), 1, now(), 1, true, true, true, true, true, true, true, true, true, true);
