04369376a9c0d151d7736c4275cb97790ffa5915
mubarismdeen <mubarisv@gmail.com>
2023-11-24 23:06:26 +0530

    added swagger ui

--- src/main/java/com/project/warehouse/express/WarehouseApplication.java
+++ src/main/java/com/project/warehouse/express/WarehouseApplication.java
@@ -2,7 +2,6 @@ package com.project.warehouse.express;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import liquibase.integration.spring.SpringLiquibase;
 
 @SpringBootApplication
 public class WarehouseApplication {
